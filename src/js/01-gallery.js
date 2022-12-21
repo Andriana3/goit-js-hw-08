4c3fb8fd600884e8bbeb3e0a221f342adc102e3c
Andriana3 <andrianavovchok@gmail.com>
2022-12-21 16:06:06 +0100

    update

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,29 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const galleryContainer = document.querySelector(".gallery");
+
+createGalleryMarkup();
+
+
+ new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  close: true,
+});
+
+function createGalleryMarkup() {
+    const galleryItemsList = galleryItems.map(({ preview, original, description }) => {
+        return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`
+    }).join(''); 
+    galleryContainer.insertAdjacentHTML('beforeend', galleryItemsList);
+    console.log(galleryItemsList);
+}
